5b1324633fb8658fb81646974c6d521196af8f01
w3d <avec1@list.ru>
2024-10-26 22:56:07 +0200

    feat: add js inf

---
+++ js/scope.js
@@ -0,0 +1,39 @@
+//Виды областей видимости
+// - Глобальная область видимости (Global Scope)
+// - Функциональная область видимости (Function Scope)
+// - Блочная область видимости (Block Scope)
+// - Лексическая область видимости (Lexical Scope)
+// - Module scope
+
+// Global Scope
+// Это область видимости, которая охватывает весь код, за пределами функций и блоков. Все переменные и функции, объявленные в глобальной области видимости, доступны везде в вашем коде.
+
+{
+  let globalVal = "I am global";
+  function showGlobal() {
+    console.log(globalVal); // "I am global"
+  }
+  showGlobal();
+}
+
+//Function Scope
+// Переменные, объявленные внутри функции, видны только внутри этой функции. Они не доступны за её пределами.
+{
+  function example() {
+    let funcVar = "I am local to this function";
+    console.log(funcVar); // "I am local to this function"
+  }
+  example();
+  console.log(funcVar); // ReferenceError: funcVar is not defined`
+}
+
+//Block Scope
+// Введён в ES6 с помощью `let` и `const`. Переменные, объявленные с помощью `let` или `const` внутри блока кода (например, внутри `{}`), видны только в этом блоке.
+{
+  if (true) {
+    let blockVar = "I am block-scoped";
+    console.log(blockVar); // "I am block-scoped"
+  }
+
+  console.log(blockVar); // ReferenceError: blockVar is not defined
+}
